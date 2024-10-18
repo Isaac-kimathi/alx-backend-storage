aa6565be881f593adce74359252e3971e4ce70f7
Kimathi isaac <Isaackimathi@gmail.com>
2024-10-18 15:44:50 +0300

    creation of a table

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,8 +1,7 @@
 -- Creates a table with unique users.
 
-DROP TABLE IF EXISTS users;
-CREATE TABLE user (
-	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	email VARCHAR(255) NOT NULL UNIQUE,
-	name VARCHAR(255)
-);
+CREATE TABLE IF NOT EXISTS users (
+	id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	email varchar(255) NOT NULL UNIQUE,
+	name varchar(255)
+	)
